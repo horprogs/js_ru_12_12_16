5034a9a7b1a5dff6eef02a3de189f7ba8d7b27c6
Georgy Perepecho <horprogs@gmail.com>
2016-12-25 18:49:05 +0300

    Task 3

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -2,11 +2,13 @@ import React, { PropTypes } from 'react'
 import ArticleList from './ArticleList'
 import UserForm from './UserForm'
 import Filter from './Filter'
+import Datepicker from './Datepicker'
 
 function AppContainer(props) {
     return (
         <div>
             <UserForm />
+            <Datepicker/>
             <Filter articles = {props.articles}/>
             <ArticleList articles={props.articles}/>
         </div>
